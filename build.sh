9be0e2bc6f018444896d99b42746234c8c521a59
shellrock <hanwoody@gmail.com>
2016-01-29 20:37:34 +0800

    build from aws

--- build.sh
+++ build.sh
@@ -9,20 +9,18 @@ export TAGS="1.625.2"
 export BASE_IMAGE=microimages/jre
 JENKINS_WAR=/root/downloads/jenkins.war
 
-docker pull $REG_URL/$BASE_IMAGE
-
-docker tag -f $REG_URL/$BASE_IMAGE $BASE_IMAGE
+docker pull $BASE_IMAGE
 
 #cp $JENKINS_WAR ./
 
-docker build -t $REG_URL/microimages/$IMAGE .
+docker build -t microimages/$IMAGE .
 
 #./test.sh
 
-echo "---> Starting push $REG_URL/microimages/$IMAGE:$VERSION"
+echo "---> Starting push microimages/$IMAGE:$VERSION"
 
 for t in $TAGS; do
-  docker tag -f $REG_URL/microimages/$IMAGE $REG_URL/microimages/$IMAGE:$t
+  docker tag -f microimages/$IMAGE microimages/$IMAGE:$t
 done
 
-docker push $REG_URL/microimages/$IMAGE
+docker push microimages/$IMAGE
