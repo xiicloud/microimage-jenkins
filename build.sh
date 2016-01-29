60b9ae1c1ebc0154c0fc7518b69e302f13a69fd4
shellrock <hanwoody@gmail.com>
2016-01-29 21:15:25 +0800

    from jdk

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ export CON_NAME=jenkins_t
 export REG_URL=d.nicescale.com:5000
 export IMAGE=jenkins
 export TAGS="1.625.2"
-export BASE_IMAGE=microimages/jre
+export BASE_IMAGE=microimages/jdk
 JENKINS_WAR=/root/downloads/jenkins.war
 
 docker pull $BASE_IMAGE
