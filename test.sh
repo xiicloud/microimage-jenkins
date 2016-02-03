9e55e34ac4f861a8589b8844a17f24508f69aa93
shellrock <hanwoody@gmail.com>
2016-02-03 15:14:04 +0800

    fix test

--- test.sh
+++ test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-set -e
+set -xe
 
 docker rm -f "$CON_NAME" > /dev/null 2>&1 || true
 docker run -d --name $CON_NAME $IMAGE
 
 docker exec $CON_NAME ps ax|grep -i "jenkin[s]"
 sleep 10
-docker exec $CON_NAME wget -O - http://localhost/
+docker exec $CON_NAME wget -O - http://localhost:8080/|grep -i jenkins
 
 docker rm -f $CON_NAME
 
